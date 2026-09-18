350af0af723c4447005885541cccb22825c48112
mikolajdobucki1-commits <mikolaj.dobucki1@gmail.com>
2026-09-18 22:53:13 +0200

    Add files via upload

---
+++ src/main/java/com/example/cpsmod/CpsMod.java
@@ -0,0 +1,10 @@
+package com.example.cpsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CpsMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        CpsHud.register();
+    }
+}
